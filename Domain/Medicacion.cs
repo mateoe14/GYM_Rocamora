9931219d80cde5cbc9105dcaed16412c8b7246ce
mateoe14 <mateoelizalde14@gmail.com>
2018-08-14 12:26:13 -0300

    confirmado

---
+++ Domain/Medicacion.cs
@@ -0,0 +1,17 @@
+public class Medicacion
+{
+    //ATRIBUTOS
+    private String nombre {get; set;};
+
+    //CONSTRUCTOR
+    public Medicacion(){}
+
+    public Medicacion(String pNombre)
+    {
+        this.nombre = pNombre;
+    }
+
+
+
+
+}
