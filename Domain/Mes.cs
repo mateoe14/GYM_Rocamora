f4fe54a05029ba9c2efbb2e7172e77fe1e559c21
mateoe14 <mateoelizalde14@gmail.com>
2018-08-14 12:48:39 -0300

    mateoe14

--- Domain/Mes.cs
+++ Domain/Mes.cs
@@ -1,16 +1,16 @@
-enum Mes
+public enum Mes
 {
-    Ene,
-    Feb,
-    Feb2,
-    Mar,
-    Abr,
-    May,
-    Jun,
-    Jul,
-    Ago,
-    Sep,
-    Oct,
-    Nov,
-    Dic
+    Ene = 1,
+    Feb = 2,
+    Feb2 = 13,
+    Mar = 3,
+    Abr = 4,
+    May = 5,
+    Jun = 6,
+    Jul = 7,
+    Ago = 8,
+    Sep = 9,
+    Oct = 10,
+    Nov = 11,
+    Dic = 12
 }
